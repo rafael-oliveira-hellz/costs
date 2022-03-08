a89d935764ea44ae6e698532657dc3e05c135c44
Rafael Oliveira <rdeoliveirasilva3@gmail.com>
2022-03-08 01:20:02 -0300

    + Implement the deletion method

--- src/components/layout/Loading.js
+++ src/components/layout/Loading.js
@@ -5,7 +5,7 @@ const Loading = () => {
     return (
         <div className={styles.loader_container}>
             <img className={styles.loader} src={loading} alt="Loading" />
-        </div>>
+        </div>
     )
 }
 
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,11 +4,14 @@ import Container from "../layout/Container";
 import Message from "../layout/Message";
 import LinkButton from "../layout/LinkButton";
 import ProjectCard from "../project/ProjectCard";
+import Loading from "../layout/Loading";
 import styles from './Projects.module.css';
 
 const Projects = () => {
 
     const [projects, setProjects] = useState([]);
+    const [removeLoading, setRemoveLoading] = useState(false);
+    const [projectMessage, setProjectMessage] = useState('');
 
     const location = useLocation();
 
@@ -28,9 +31,25 @@ const Projects = () => {
         .then(resp => resp.json())
         .then(data => {
             setProjects(data)
+            setRemoveLoading(true)
         })
         .catch((err) => console.log(err))
     }, [])
+
+    function removeProject(id) {
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        .then(resp => resp.json())
+        .then(() => {
+            setProjects(projects.filter((project) => project.id !== id))
+            setProjectMessage('Projeto removido com sucesso!')
+        })
+        .catch((err) => console.log(err))
+    }
 
     return (
         <div className={styles.project_container}>
@@ -41,6 +60,7 @@ const Projects = () => {
             </div>
 
             { message && <Message type="success" msg={ message } /> }
+            { projectMessage && <Message type="error" msg={ projectMessage } /> }
 
             <Container customClass="start">
                 {projects.length > 0 && 
@@ -51,7 +71,13 @@ const Projects = () => {
                     budget={project.budget}
                     category={project.category.name}
                     key={project.id}
+                    handleRemove={removeProject}
                     />
+                )}
+
+                { !removeLoading && <Loading /> }
+                { removeLoading && projects.length === 0 && (
+                    <p>Não há projetos cadastrados!</p>
                 )}
             </Container>
         </div>
